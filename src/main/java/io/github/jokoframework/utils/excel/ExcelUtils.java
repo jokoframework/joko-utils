a476a3b57c91217119163004d6436927ee6a8efa
Ale Feltes <afeltes@sodep.com.py>
2019-04-16 14:28:05 -0400

    Arregla issues reportados por el sonarqube

--- src/main/java/io/github/jokoframework/utils/excel/ExcelUtils.java
+++ src/main/java/io/github/jokoframework/utils/excel/ExcelUtils.java
@@ -19,7 +19,6 @@ import io.github.jokoframework.utils.exception.JokoUtilsException;
 
 /**
  * Excel Generator
- * 
  */
 public class ExcelUtils {
 
@@ -28,81 +27,76 @@ public class ExcelUtils {
      * la dirección mas nombre del archivo que se creara (/home/user/mi_excel.xls), al finalizar el método se
      * habrá creado un archivo Excel con los datos, dirección y nombre especificados.
      *
-     * @param data Tabla con los datos a incluir en el archivo Excel final
-     * @param header Lista con los nombres de cada columna (En orden de aparición)
+     * @param data     Tabla con los datos a incluir en el archivo Excel final
+     * @param header   Lista con los nombres de cada columna (En orden de aparición)
      * @param fileName Nombre del archivo Excel final, incluyendo el camino
      * @throws JokoUtilsException Excepción de Joko Utils
      */
-	public void generateExcel(Table<Integer, Integer, Object> data,
-			List<String> header, String fileName) throws JokoUtilsException {
-		generateReportFile(data, header, fileName);
-	}
+    public void generateExcel(Table<Integer, Integer, Object> data,
+                              List<String> header, String fileName) throws JokoUtilsException {
+        generateReportFile(data, header, fileName);
+    }
 
     /**
      * Recibe una tabla con datos, una lista los nombres de las columnas de la tabla (Nombres en orden de aparición) y
      * la dirección mas nombre del archivo que se creara (/home/user/mi_excel.xls), al finalizar el método se
      * habrá creado un archivo Excel con los datos, dirección y nombre especificados.
      *
-     * @param data Tabla con los datos a incluir en el archivo Excel final
-     * @param header Lista con los nombres de cada columna (En orden de aparición)
+     * @param data     Tabla con los datos a incluir en el archivo Excel final
+     * @param header   Lista con los nombres de cada columna (En orden de aparición)
      * @param fileName Nombre del archivo Excel final, incluyendo el camino
      * @throws JokoUtilsException Excepción de Joko Utils
      */
     public void generateReportFile(Table<Integer, Integer, Object> data, List<String> header, String fileName)
             throws JokoUtilsException {
-        int rowCount = 0;
+
         // 1. Create a new Workbook
-        Workbook wb = new XSSFWorkbook();
-        // 2. Create a new sheet
-        Sheet sheet = wb.createSheet("sheet 1");
-        Object registro = null;
-        if (!data.isEmpty()) {
-            Row row = sheet.createRow(rowCount);
-            for (int i = 0; i < header.size(); i++) {
-                // 3. Create a header row
-                row.createCell(i).setCellValue(header.get(i));
-            }
-        } else {
-            throw new JokoUtilsException("Empty data set");
-        }
-
-        Iterator<Integer> rows = data.rowKeySet().iterator();
-        while (rows.hasNext()) {
-            rowCount += 1;
-            Row row = sheet.createRow(rowCount);
-            Integer rowNumber = rows.next();
-            Map<Integer, Object> currentRow = data.row(rowNumber);
-            Iterator<Integer> rowDataIterator = currentRow.keySet().iterator();
-            while (rowDataIterator.hasNext()) {
-                Integer colNumber = rowDataIterator.next();
-                registro = currentRow.get(colNumber);
-                // TODO: Format according cell type
-                if (registro instanceof Number) {
-                    row.createCell(colNumber).setCellValue(
-                            Double.parseDouble(registro.toString()));
-                } else if (registro instanceof Date) {
-                    row.createCell(colNumber).setCellValue((Date) registro);
-                } else if (registro instanceof Boolean) {
-                    row.createCell(colNumber).setCellValue((Boolean) registro);
-                } else {
-                    row.createCell(colNumber).setCellValue(registro.toString());
+        try (Workbook wb = new XSSFWorkbook()) {
+            int rowCount = 0;
+            // 2. Create a new sheet
+            Sheet sheet = wb.createSheet("sheet 1");
+            Object registro = null;
+            if (!data.isEmpty()) {
+                Row row = sheet.createRow(rowCount);
+                for (int i = 0; i < header.size(); i++) {
+                    // 3. Create a header row
+                    row.createCell(i).setCellValue(header.get(i));
                 }
-            }// End column iteration
-        }// End row iteration
-        // 5. create excel file
-        FileOutputStream fileOut;
-        try {
+            } else {
+                throw new JokoUtilsException("Empty data set");
+            }
 
-            fileOut = new FileOutputStream(fileName);
-            wb.write(fileOut);
-            fileOut.close();
+            Iterator<Integer> rows = data.rowKeySet().iterator();
+            while (rows.hasNext()) {
+                rowCount += 1;
+                Row row = sheet.createRow(rowCount);
+                Integer rowNumber = rows.next();
+                Map<Integer, Object> currentRow = data.row(rowNumber);
+                Iterator<Integer> rowDataIterator = currentRow.keySet().iterator();
+                while (rowDataIterator.hasNext()) {
+                    Integer colNumber = rowDataIterator.next();
+                    registro = currentRow.get(colNumber);
+                    // TODO: Format according cell type
+                    if (registro instanceof Number) {
+                        row.createCell(colNumber).setCellValue(
+                                Double.parseDouble(registro.toString()));
+                    } else if (registro instanceof Date) {
+                        row.createCell(colNumber).setCellValue((Date) registro);
+                    } else if (registro instanceof Boolean) {
+                        row.createCell(colNumber).setCellValue((Boolean) registro);
+                    } else {
+                        row.createCell(colNumber).setCellValue(registro.toString());
+                    }
+                }// End column iteration
+            }// End row iteration
+            // 5. create excel file
 
-        } catch (FileNotFoundException e) {
-            throw new JokoUtilsException(e);
+            try (FileOutputStream fileOut = new FileOutputStream(fileName)){
+                wb.write(fileOut);
+            }
         } catch (IOException e) {
             throw new JokoUtilsException(e);
         }
-        
     }
-    
+
 }
